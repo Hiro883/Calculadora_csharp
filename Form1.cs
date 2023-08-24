f0028fe7e0e135337a598ab4f4a7f446fb5e0f09
Angel <mc21081@ues.edu.sv>
2023-08-23 19:12:59 -0600

    second commit

--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,13 @@ using System.Windows.Forms;
 
 namespace ProyectoInv1
 {
-    public partial class Calculadora : Form
+    public partial class CalculadoraPRN115 : Form
     {
-        public Calculadora()
+        //variables publicas
+        double Numero1 = 0, Numero2 = 0;
+        char Operador;
+
+        public CalculadoraPRN115()
         {
             InitializeComponent();
         }
@@ -22,92 +26,84 @@ namespace ProyectoInv1
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void agregarNumero(object sender, EventArgs e)
         {
+            var boton = ((Button)sender);
 
+            if (txtResultado.Text == "0")
+                txtResultado.Text = "";
+                txtResultado.Text += boton.Text;
         }
 
-        private void btn9_Click(object sender, EventArgs e)
+        private void btnResultado_Click(object sender, EventArgs e)
         {
-            txtResultado.Text = "9";
-        }
+            
+            Numero2 = Convert.ToDouble(txtResultado.Text);
 
-        private void btnRaiz_Click(object sender, EventArgs e)
-        {
-
+            if (Operador == '+')
+            {
+                txtResultado.Text = (Numero1 + Numero2).ToString();
+                Numero1 = Convert.ToDouble(txtResultado.Text);
+            }
+            else if (Operador == '-')
+            {
+                txtResultado.Text = (Numero1 - Numero2).ToString();
+                Numero1 = Convert.ToDouble(txtResultado.Text);
+            }
+            else if (Operador == 'x')
+            {
+                txtResultado.Text = (Numero1 * Numero2).ToString();
+                Numero1 = Convert.ToDouble(txtResultado.Text);
+            }
+            else if (Operador == '/')
+            {
+                txtResultado.Text = (Numero1 / Numero2).ToString();
+                Numero1 = Convert.ToDouble(txtResultado.Text);
+            }
         }
 
         private void btnBorrar_Click(object sender, EventArgs e)
         {
-
+            if (txtResultado.Text.Length > 1)
+            {
+                txtResultado.Text = txtResultado.Text.Substring(0, txtResultado.Text.Length - 1);
+            }
+            else
+            {
+                txtResultado.Text = "0";
+            }
         }
 
         private void btnBorrarTodo_Click(object sender, EventArgs e)
         {
-
+            txtResultado.Clear();
         }
 
-        private void btn0_Click(object sender, EventArgs e)
+        private void clickOperador(object sender, EventArgs e)
         {
+            var boton = ((Button)sender);
+            Numero1 = Convert.ToDouble(txtResultado.Text);
+            Operador = Convert.ToChar(boton.Tag);
 
+            //Numero al cuadrado
+            if (Operador == '²')
+            {
+                txtResultado.Text = Math.Pow(Numero1, 2).ToString();
+                //Numero1 = Convert.ToDouble(txtResultado.Text);
+            }
+            ///Sacar raiz cuadrada
+            else if (Operador == '√')
+            {
+                txtResultado.Text = Math.Sqrt(Numero1).ToString();
+                //Numero1 = Convert.ToDouble(txtResultado.Text);
+            }
+            else
+            {
+                txtResultado.Text = "0";
+            }
+            
         }
 
-        private void btnIgual_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnPunto_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtA_TextChanged(object sender, EventArgs e)
-        {
-            if (txtResultado.Text == "0")
-                txtResultado.Text = "";
-
-            txtResu
-        }
+        
     }
 }
